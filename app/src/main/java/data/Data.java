5e34b76bb433dc5591b3e6700f17600bf07b406b
UllasJain <ullasjain21@gmail.com>
2017-10-24 19:40:54 +0530

    adding anim files

---
+++ app/src/main/java/data/Data.java
@@ -0,0 +1,17 @@
+package data;
+
+/**
+ * Created by UllasJain on 01/06/17.
+ */
+
+public class Data {
+
+    public String name;
+    public boolean toAnimate;
+    public boolean isBothNotPresent;
+    public float x = 0;
+
+    public Data(String name) {
+        this.name = name;
+    }
+}
